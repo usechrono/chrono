d78cd883f9a9bbf83e10c63e88821a4046adeb90
Rohit Singh <rohit.singh.33521@gmail.com>
2025-08-11 01:30:35 +0530

    Add input params to workflows

--- chrono/workflow.go
+++ chrono/workflow.go
@@ -7,7 +7,7 @@ import (
 // Step represents a single action in a workflow
 type Step struct {
 	Name string
-	Run  func() error
+	Run  func(params map[string]interface{}) error
 }
 
 // Workflow represents a series of steps to execute
@@ -17,14 +17,14 @@ type Workflow struct {
 }
 
 // Run executes all steps in the workflow sequentially
-func (w *Workflow) Run() error {
+func (w *Workflow) Run(params map[string]interface{}) {
 	fmt.Printf("Starting workflow: %s\n", w.Name)
-	for i, s := range w.Steps {
-		fmt.Printf("  [%d/%d] %s\n", i+1, len(w.Steps), s.Name)
-		if err := s.Run(); err != nil {
-			return fmt.Errorf("step %q failed: %w", s.Name, err)
+	for i, step := range w.Steps {
+		fmt.Printf("  [%d/%d] %s\n", i+1, len(w.Steps), step.Name)
+		if err := step.Run(params); err != nil {
+			fmt.Printf("    ❌ Error: %v\n", err)
+			break
 		}
 	}
 	fmt.Printf("Workflow %s completed\n", w.Name)
-	return nil
 }
